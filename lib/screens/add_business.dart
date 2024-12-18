38f7b34cedc6a36f735966d33fc0fb95e66ae884
nishamindcodes <nisham@mindcodes.in>
2024-12-18 17:26:45 +0530

    11 - app bar and bottom navigation bar is added to the screen

--- lib/screens/add_business.dart
+++ lib/screens/add_business.dart
@@ -1,3 +1,5 @@
+import 'package:bottle_crush/widgets/custom_app_bar.dart';
+import 'package:bottle_crush/widgets/custom_bottom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class AddBusiness extends StatefulWidget {
@@ -8,8 +10,23 @@ class AddBusiness extends StatefulWidget {
 }
 
 class _AddBusinessState extends State<AddBusiness> {
+  int _selectedIndex = 0; // Track the selected index for bottom nav items
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // Handle navigation logic here based on the index
+    print('Selected Index: $index'); // Example print statement
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const CustomAppBar(), // Use the custom app bar widget here
+      bottomNavigationBar: CustomBottomAppBar(
+        onItemTapped: _onItemTapped, // Pass the callback
+      ),
+    );
   }
 }
