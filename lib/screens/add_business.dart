8c9a6025a1b6050b9ce267ea7f736e1409b932d4
nishamindcodes <nisham@mindcodes.in>
2024-12-20 20:19:17 +0530

    11 - UI design completed

--- lib/screens/add_business.dart
+++ lib/screens/add_business.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../widgets/custom_elevated_button.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io'; // <-- Add this import for File operations
 
 class AddBusiness extends StatefulWidget {
   const AddBusiness({super.key});
@@ -17,14 +18,13 @@ class _AddBusinessState extends State<AddBusiness> {
   int _selectedIndex = 0; // Track the selected index for bottom nav items
   bool _isPasswordVisible = false;
   final TextEditingController _businessNameController = TextEditingController();
-  final TextEditingController _businessEmailController =
-      TextEditingController();
-  final TextEditingController _businessMobileController =
-      TextEditingController();
-  final TextEditingController _businessPasswordController =
-      TextEditingController();
+  final TextEditingController _businessEmailController = TextEditingController();
+  final TextEditingController _businessMobileController = TextEditingController();
+  final TextEditingController _businessPasswordController = TextEditingController();
+  final TextEditingController _logoPathController = TextEditingController();
 
   final ImagePicker _picker = ImagePicker();
+  File? _selectedImage; // Variable to hold the selected image file
 
   void _onItemTapped(int index) {
     setState(() {
@@ -35,13 +35,18 @@ class _AddBusinessState extends State<AddBusiness> {
   }
 
   @override
+  void dispose() {
+    _logoPathController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: const CustomAppBar(),
-      backgroundColor:
-          AppTheme.backgroundWhite, // Use the custom app bar widget here
+      backgroundColor: AppTheme.backgroundWhite, // Use the custom app bar widget here
       body: Padding(
         padding: const EdgeInsets.only(left: 12.0, top: 8.0, right: 12.0),
         child: Column(
@@ -60,14 +65,12 @@ class _AddBusinessState extends State<AddBusiness> {
             ),
             // 1
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
               child: TextFormField(
                 controller: _businessNameController,
                 style: TextStyle(
                     fontSize: screenWidth * 0.03,
-                    color: AppTheme
-                        .textBlack), // Set the font size for entered text
+                    color: AppTheme.textBlack), // Set the font size for entered text
                 decoration: InputDecoration(
                   labelText: 'Business Name',
                   labelStyle: TextStyle(fontSize: screenWidth * 0.03),
@@ -84,14 +87,12 @@ class _AddBusinessState extends State<AddBusiness> {
             ),
             //2
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
               child: TextFormField(
                 controller: _businessEmailController,
                 style: TextStyle(
                     fontSize: screenWidth * 0.03,
-                    color: AppTheme
-                        .textBlack), // Set the font size for entered text
+                    color: AppTheme.textBlack), // Set the font size for entered text
                 decoration: InputDecoration(
                   labelText: 'Business Email',
                   labelStyle: TextStyle(fontSize: screenWidth * 0.03),
@@ -108,15 +109,13 @@ class _AddBusinessState extends State<AddBusiness> {
             ),
             //3
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
               child: TextFormField(
                 controller: _businessMobileController,
                 keyboardType: TextInputType.number,
                 style: TextStyle(
                     fontSize: screenWidth * 0.03,
-                    color: AppTheme
-                        .textBlack), // Set the font size for entered text
+                    color: AppTheme.textBlack), // Set the font size for entered text
                 decoration: InputDecoration(
                   labelText: 'Business Mobile',
                   labelStyle: TextStyle(fontSize: screenWidth * 0.03),
@@ -133,15 +132,13 @@ class _AddBusinessState extends State<AddBusiness> {
             ),
             //4
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
               child: TextFormField(
                 controller: _businessPasswordController,
                 obscureText: !_isPasswordVisible,
                 style: TextStyle(
                     fontSize: screenWidth * 0.03,
-                    color: AppTheme
-                        .textBlack), // Set the font size for entered text
+                    color: AppTheme.textBlack), // Set the font size for entered text
                 decoration: InputDecoration(
                   labelText: 'Password',
                   labelStyle: TextStyle(fontSize: screenWidth * 0.03),
@@ -171,14 +168,13 @@ class _AddBusinessState extends State<AddBusiness> {
             ),
             //5
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
               child: TextFormField(
-                //controller: _businessMobileController,
+                controller: _logoPathController,
                 style: TextStyle(
-                    fontSize: screenWidth * 0.03,
-                    color: AppTheme
-                        .textBlack), // Set the font size for entered text
+                  fontSize: screenWidth * 0.03,
+                  color: AppTheme.textBlack,
+                ), // Set the font size for entered text
                 decoration: InputDecoration(
                   labelText: 'Logo',
                   labelStyle: TextStyle(fontSize: screenWidth * 0.03),
@@ -196,14 +192,16 @@ class _AddBusinessState extends State<AddBusiness> {
                     onPressed: () async {
                       // Open image picker on click
                       final XFile? image = await _picker.pickImage(
-                        source: ImageSource
-                            .gallery, // You can use ImageSource.camera to pick from the camera
+                        source: ImageSource.gallery, // You can use ImageSource.camera to pick from the camera
                       );
 
                       if (image != null) {
                         // Image selected
+                        setState(() {
+                          _selectedImage = File(image.path); // Save the selected image file
+                          _logoPathController.text = image.path; // Update the TextField
+                        });
                         print('Selected image: ${image.path}');
-                        // Handle the image here (e.g., display it, upload it, etc.)
                       } else {
                         // User canceled the picker
                         print('Image selection canceled.');
@@ -216,11 +214,51 @@ class _AddBusinessState extends State<AddBusiness> {
                 ),
               ),
             ),
+            // Display selected image if available
+            if (_selectedImage != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: Stack(
+                  children: [
+                    Image.file(
+                      File(_selectedImage!.path),
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap: () {
+                          // Remove the selected image
+                          setState(() {
+                            _selectedImage = null;
+                            _logoPathController.clear(); // Clear the TextField
+                          });
+                        },
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: AppTheme.backgroundBlue,
+                            shape: BoxShape.circle,
+                          ),
+                          padding: const EdgeInsets.all(4.0),
+                          child: const Icon(
+                            Icons.close,
+                            size: 16.0,
+                            color: AppTheme.backgroundWhite,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
 
             // Add the Cancel and Submit buttons at the bottom of the screen
             const Spacer(), // Push buttons to the bottom
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
+            Padding(padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -228,26 +266,24 @@ class _AddBusinessState extends State<AddBusiness> {
                     buttonText: 'Cancel',
                     onPressed: () {},
                     width: screenWidth * 0.4,
-                    backgroundColor:
-                        AppTheme.backgroundWhite, // White background for Cancel
-                    textColor:
-                        AppTheme.backgroundBlue, // Blue text for Cancel button
-                    borderColor: AppTheme
-                        .backgroundBlue, // Blue border for Cancel button
-                    icon: const Icon(Icons.cancel,
-                        color:
-                            AppTheme.backgroundBlue), // Cancel icon as prefix
+                    backgroundColor: AppTheme.backgroundWhite, // White background for Cancel
+                    textColor: AppTheme.backgroundBlue, // Blue text for Cancel button
+                    borderColor: AppTheme.backgroundBlue, // Blue border for Cancel button
+                    icon: const Icon(Icons.cancel, color: AppTheme.backgroundBlue), // Cancel icon as prefix
                   ),
                   CustomElevatedButton(
                     buttonText: 'Submit',
-                    onPressed: () {},
+                    onPressed: () {
+                      // Handle the submit action
+                      if (_selectedImage != null) {
+                        // Example: upload the image to server or handle the selected image
+                        print('Selected Image: ${_selectedImage?.path}');
+                      }
+                    },
                     width: screenWidth * 0.4,
-                    backgroundColor:
-                        AppTheme.backgroundBlue, // Blue background for Submit
-                    textColor:
-                        AppTheme.textWhite, // White text for Submit button
-                    icon: const Icon(Icons.check,
-                        color: AppTheme.textWhite), // Check icon as prefix
+                    backgroundColor: AppTheme.backgroundBlue, // Blue background for Submit
+                    textColor: AppTheme.textWhite, // White text for Submit button
+                    icon: const Icon(Icons.check, color: AppTheme.textWhite), // Check icon as prefix
                   ),
                 ],
               ),
